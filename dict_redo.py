6df976a7d645c37cc0f3edb1e28d8b4776f38028
Roman Herasymenko <roman.herasymenko@gmail.com>
2023-06-18 17:01:50 +0200

    Refactored. Added _reverse_action.

--- dict_redo.py
+++ dict_redo.py
@@ -23,7 +23,13 @@ class RedoDictionary():
         del self._dict[key]
 
     def undo(self):
-        action, key, old_value = self._stack_undo.pop()
+        self._reverse_action(self._stack_undo, self._stack_redo)
+
+    def redo(self):
+        self._reverse_action(self._stack_redo, self._stack_undo)
+
+    def _reverse_action(self, input_stack, output_stack):
+        action, key, old_value = input_stack.pop()
 
         # 1. action set and we have a key inside the dictionary tren we need to save the current value to redo and restore the old one
         # 2. action set and we do not have the key in the dictionary then we need to save delete to redo
@@ -33,34 +39,18 @@ class RedoDictionary():
             # Saving the current value to the redo list before we reset it
             # to the previous value
 
-            self._stack_redo.append(('set', key, self._dict[key]))
+            output_stack.append(('set', key, self._dict[key]))
             self._dict[key] = old_value
 
         elif action == 'set' and key not in self._dict:
             # We reverse delete action, and we need to save it for redo
 
-            self._stack_redo.append(('delete', key, old_value))
+            output_stack.append(('delete', key, old_value))
             self._dict[key] = old_value
 
         else:
             # Otherwise we need to delete
             # and to add a set action to the redo list.
 
-            self._stack_redo.append(('set', key, old_value))
-            del self._dict[key]
-
-    def redo(self):
-        # TODO: undo and redo pretty much the same. It can be separated into a separated method
-        action, key, old_value = self._stack_redo.pop()
-
-        if action == 'set' and key in self._dict:
-            self._stack_undo.append(('set', key, self._dict[key]))
-            self._dict[key] = old_value
-
-        elif action == 'set' and key not in self._dict:
-            self._stack_undo.append(('delete', key, old_value))
-            self._dict[key] = old_value
-
-        else:
-            self._stack_undo.append(('set', key, self._dict[key]))
+            output_stack.append(('set', key, old_value))
             del self._dict[key]
